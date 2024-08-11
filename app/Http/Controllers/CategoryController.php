f6704d9222ee24307c85011863f02619cc5821b4
unknown <mahendrayudapradana@mail.ugm.ac.id>
2024-08-12 05:28:04 +0700

    update categories to unique

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -32,7 +32,7 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'category' => 'required|string|max:255',
+            'category' => 'required|string|max:255|unique:categories',
         ]);
 
         Categories::create([
